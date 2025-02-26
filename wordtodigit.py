a3e1d04bc919c4e5746c9f249b221e786470c878
Adeleke Adeoluwa <88342625+Davico007@users.noreply.github.com>
2025-02-26 20:33:00 +0100

    enabled text-to-speech

--- wordtodigit.py
+++ wordtodigit.py
@@ -1,3 +1,17 @@
+from playsound3 import playsound
+
+zero = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/0.wav"
+one = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/1.wav"
+two = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/2.wav"
+three = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/3.wav"
+four = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/4.wav"
+five = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/5.wav"
+six = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/6.wav"
+seven = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/7.wav"
+eight = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/8.wav"
+nine = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/9.wav"
+error = "C:/Users/PC/PycharmProjects/PythonProject/HelloWorld/audio-numbers/error-126627.mp3"
+
 digits_mapping = {
     '1': 'One',
     '2': 'Two',
@@ -9,6 +23,18 @@ digits_mapping = {
     '8': 'Eight',
     '9': 'Nine',
     '0': 'Zero'
+}
+sound_mapping= {
+    '1': one,
+    '2': two,
+    '3': three,
+    '4': four,
+    '5': five,
+    '6': six,
+    '7': seven,
+    '8': eight,
+    '9': nine,
+    '0': zero
 }
 
 words_mapping = {
@@ -23,6 +49,9 @@ words_mapping = {
     'nine': '9',
     'zero': '0'
 }
+
+
+
 is_digit = bool
 print('Welcome! What would you like?')
 multiple= 1
@@ -33,9 +62,14 @@ while True:
         print("You have selected Digits to Words.")
         digits = input('Phone: ')
         output = ''
+        sound = []
         for character in digits:
+            sound.append(sound_mapping.get(character, error))
             output += digits_mapping.get(character, '!') + ' '
         print(f"Result: {output}")
+        # print(sound)
+        for x in sound:
+            playsound(x)
     elif command == 'w':
         is_digit = False
         print("You have selected Words to Digits.")
@@ -66,6 +100,7 @@ while True:
 
                 iteration = int(cmd)
                 display = ''
+                sound = []
                 for x in range(iteration):
 
                     word = input(f'Word {multiple}: ').lower()
@@ -75,6 +110,10 @@ while True:
                 for word in folder:
                     display += words_mapping.get(word, '!')
                 print(f'Result: {display}')
+                for num in display:
+                    sound.append(sound_mapping.get(num, error))
+                for x in sound:
+                    playsound(x)
                 multiple = 1
                 # repeat_add(iteration, do)
                 break
