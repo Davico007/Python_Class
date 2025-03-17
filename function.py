77ae39a0d0f1dfdc3b49a1917f65b0f271d31913
Adeleke Adeoluwa <88342625+Davico007@users.noreply.github.com>
2025-03-17 10:46:08 +0100

    classes

---
+++ function.py
@@ -0,0 +1,34 @@
+# try:
+#     age = int(input('Age: '))
+#     income = 2000
+#     risk = income/age
+#     print(age)
+# except ValueError:
+#     print('invalid value')
+# except ZeroDivisionError:
+#     print('Age cannot be zero')
+
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def move(self):
+        print('move')
+
+
+    def draw(self):
+        print('draw')
+
+# point1 = Point()
+# point1.x = 10
+# point1.y = 20
+# print(point1.x)
+# point1.draw()
+# point2 = Point()
+# point2.x = 1
+# print(point2.x)
+
+
+point = Point(10,20)
+point.x = 11
+print(point.x)
