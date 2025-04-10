8c7bfef165f1530065933fdb15d0ddb246dadc12
Adeleke Adeoluwa <88342625+Davico007@users.noreply.github.com>
2025-04-10 23:48:41 +0100

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,9 @@
 num_of_sequence = int(input('How many sequence do you want? '))
 
-def fibonacci_sequence(amount):
+def generate_fibonacci(amount):
     fib = [0,1]
-    for n in range(num_of_sequence-2):
-        fib.append(fib[len(fib)-1] + fib[len(fib)-2])
+    for n in range(num_of_sequence - 2):
+        fib.append(fib[len(fib) - 1] + fib[len(fib) - 2])
     return fib
 
-print(fibonacci_sequence(num_of_sequence))
+print(genarate_fibonacci(num_of_sequence))
